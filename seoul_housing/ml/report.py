f10468dd6ad9b81bbce30945fdfba6b091d4f929
imsong7 <songsoo409@gmail.com>
2025-03-30 20:36:43 +0900

    Fixed:Error

--- seoul_housing/ml/report.py
+++ seoul_housing/ml/report.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import json
 import os
+import pandas as pd
 from prophet.serialize import model_from_json
 from prophet.plot import plot_plotly
 
@@ -35,12 +36,17 @@ def reportMain(total_df):
         mime="text/csv"
     )
 
-    max_row = forecast.loc[forecast['yhat'].idxmax(), ['ds', 'yhat']]
-    min_row = forecast.loc[forecast['yhat'].idxmin(), ['ds', 'yhat']]
+    max_date = forecast['ds'].max()
+    start_date = max_date - pd.Timedelta(days=periods)
+    future_data = forecast[forecast['ds'] > start_date]
 
-    max_date = forecast.loc[forecast['yhat'].idxmax(), 'ds'].strftime('%m월 %d일')
-    min_date = forecast.loc[forecast['yhat'].idxmin(), 'ds'].strftime('%m월 %d일')
-    mean_yhat = forecast['yhat'].mean()
+    max_row = future_data.loc[future_data['yhat'].idxmax(), ['ds', 'yhat']]
+    min_row = future_data.loc[future_data['yhat'].idxmin(), ['ds', 'yhat']]
+
+    max_date = future_data.loc[future_data['yhat'].idxmax(), 'ds'].strftime('%m월 %d일')
+    min_date = future_data.loc[future_data['yhat'].idxmin(), 'ds'].strftime('%m월 %d일')
+    mean_yhat = future_data['yhat'].mean()
+
 
     st.markdown(f"### 📍 {cgg_nm} 향후 {periods}일간 아파트 가격 예측")
     st.markdown(f"#### 평균 가격은 {mean_yhat:,.0f}만원")
