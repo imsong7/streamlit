c7515d816e110bb6a880594d7e513207d4cf0299
imsong7 <songsoo409@gmail.com>
2025-03-30 16:24:04 +0900

    Fixed:Error

--- seoul_housing/eda/map.py
+++ seoul_housing/eda/map.py
@@ -19,12 +19,12 @@ def set_korean_font():
 
 def mapMatplotlib(merge_df):
     font_prop = set_korean_font()
-    fig, ax = plt.subplots(ncols=3, sharey=True, figsize=(50, 18))
+    fig, ax = plt.subplots(ncols=3, sharey=True, figsize=(30, 18))
     merge_df[merge_df['month'] == 1].plot(ax=ax[0], column='mean', cmap='Pastel1', legend=False, alpha=0.9, edgecolor='gray')
     merge_df[merge_df['month'] == 2].plot(ax=ax[1], column='mean', cmap='Pastel1', legend=False, alpha=0.9, edgecolor='gray')
     merge_df[merge_df['month'] == 3].plot(ax=ax[2], column='mean', cmap='Pastel1', legend=False, alpha=0.9, edgecolor='gray')
 
-    plt.subplots_adjust(wspace=0.07) 
+    plt.subplots_adjust(wspace=0.05) 
 
     patch_col = ax[0].collections[0]
     cb = fig.colorbar(patch_col, ax=ax, orientation='horizontal', shrink=0.5, aspect=50, pad=0.1)
