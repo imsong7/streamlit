85bb54a8f8d9bb37b8097709852f7f7a68591770
imsong7 <songsoo409@gmail.com>
2025-03-30 19:48:49 +0900

    Fixed:Error

--- seoul_housing/ml/predict.py
+++ seoul_housing/ml/predict.py
@@ -63,7 +63,7 @@ def load_models(cgg_nms):
     return models
 
 def predictDistrict(total_df, periods):
-    st.markdown(f"#### 📍 2025년 서울시 자치구별 평균가격 예측 {periods}일간 ")
+    st.markdown(f"#### 자치구별 ({periods}일간")
     font_prop = set_korean_font()
     cgg_nms = sorted(list(total_df['CGG_NM'].unique()))
 
@@ -104,7 +104,7 @@ def predictDistrict(total_df, periods):
     st.pyplot(fig)
 
 def predictType(total_df, periods):
-    st.markdown(f"#### 📍 2025년 서울시 주거형태별 평균가격 예측 {periods}일간 ")
+    st.markdown(f"#### 주거형태별 ({periods}일간")
     types = list(total_df['BLDG_USG'].unique())
 
     fig = predict_plot(total_df, types, periods)
@@ -112,6 +112,7 @@ def predictType(total_df, periods):
     st.pyplot(fig)
 
 def predict(total_df):
+    st.markdown(f"### 📍 2025년 서울시 평균가격 예측")
     total_df['CTRT_DAY'] = pd.to_datetime(total_df['CTRT_DAY'], format='%Y-%m-%d')
     periods = int(st.number_input("향후 예측기간을 지정하세요(1일~30일)", min_value=1, max_value=30, step=1))
 
