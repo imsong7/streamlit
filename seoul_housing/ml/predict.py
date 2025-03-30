330b90f4c9c85cdb3cd633d7f866d5bba86e52cd
imsong7 <songsoo409@gmail.com>
2025-03-30 19:43:36 +0900

    Fixed:Error

--- seoul_housing/ml/predict.py
+++ seoul_housing/ml/predict.py
@@ -68,7 +68,7 @@ def predictDistrict(total_df, periods):
     cgg_nms = sorted(list(total_df['CGG_NM'].unique()))
 
     models = load_models(cgg_nms)
-    fig, ax = plt.subplots(figsize=(30,20), sharey=False, ncols=5, nrows=5)
+    fig, ax = plt.subplots(figsize=(30,25), sharey=False, ncols=5, nrows=5)
     for i in range(len(cgg_nms)):  
         future = models[i].make_future_dataframe(periods=periods)
         forecast = models[i].predict(future)
