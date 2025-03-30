abbec04cbe7989e2dde924c5d1381be1bef411a8
imsong7 <songsoo409@gmail.com>
2025-03-30 14:07:09 +0900

    Fixed:Error

--- seoul_housing/eda/map.py
+++ seoul_housing/eda/map.py
@@ -68,7 +68,7 @@ def mapPlotly(merge_df):
         margin={"r":0, "t":50, "l":0, "b":0},
         font=dict(family=font_prop.get_name()),
         title=f"2023년 {month}월 서울시 아파트 평균가격",
-        title_font=dict(family=font_prop.get_name(), size=20, weight='light')  # Title settings
+        title_font=dict(family=font_prop.get_name(), size=20, weight='normal')  # Title settings
     )
     
     fig.update_traces(hovertemplate='<b>%{location}</b><br>아파트평균가격: %{z:,.0f}(만원)')
