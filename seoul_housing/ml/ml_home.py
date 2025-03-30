c4741fe131b3bc89d12c48151913ff75ff635ab1
imsong7 <songsoo409@gmail.com>
2025-03-30 14:50:00 +0900

    Fixed: Error

--- seoul_housing/ml/ml_home.py
+++ seoul_housing/ml/ml_home.py
@@ -10,11 +10,12 @@ from ml.report import reportMain
 
 def home():
     st.markdown("### 머신러닝 예측 개요 \n"
-                "- 가구당 예측 그래프 추세 \n"
-                "- 자치구역별 예측 그래프 추세 \n"
-                "- 사용된 알고리즘 소개 \n"
-                "   + Facebook Prophet 알고리즘 사용\n"
-                "   + 출처 : hpp")
+            "- 가구당 예측 그래프 추세: 가구당 예측 결과를 시간에 따라 시각화하여 예측된 변화 추이를 확인합니다. \n"
+            "- 자치구역별 예측 그래프 추세: 각 자치구별 예측 결과를 시각화하여 지역별 변화를 분석합니다. \n"
+            "- 사용된 알고리즘 소개: \n"
+            "   + **Facebook Prophet** 알고리즘: 시계열 예측에 효과적인 모델로, 계절성과 트렌드를 자동으로 학습하고 예측합니다. \n"
+            "   + **출처**: https://arxiv.org/pdf/2303.01903")
+
 
 def run_ml(total_df):
     total_df['CTRT_DAY'] = pd.to_datetime(total_df['CTRT_DAY'], format='%Y-%m-%d')
