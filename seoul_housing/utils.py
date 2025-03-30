f1ebd96491ff29349b345df4fabf33e831847e1e
imsong7 <songsoo409@gmail.com>
2025-03-30 16:05:30 +0900

    Fixed:Error

--- seoul_housing/utils.py
+++ seoul_housing/utils.py
@@ -4,8 +4,7 @@ import streamlit as st
 import pandas as pd
 
 # 데이터 캐싱 함수로, 데이터 불러오기 이후 메모리에 저장
-@st.cache_data
+@st.cache_data(ttl=3600, max_entries=5)
 def load_data():
-    data =pd.read_csv('seoul_housing/data/seoul_real_estate.csv')
-                       
+    data = pd.read_csv('seoul_housing/data/seoul_real_estate.csv')
     return data
