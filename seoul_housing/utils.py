4befd1a4de9281842d341b2ad41e7604e0e8ab32
imsong7 <songsoo409@gmail.com>
2025-04-01 20:13:06 +0900

    Updated

--- seoul_housing/utils.py
+++ seoul_housing/utils.py
@@ -7,8 +7,8 @@ import pandas as pd
 @st.cache_data(ttl=3600, max_entries=5)
 def load_data(lang):
     if lang == 'English':
-        file_path = 'data/seoul_real_estate_en.csv'
+        file_path = 'seoul_housing/data/seoul_real_estate_en.csv'
     else:
-        file_path = 'data/seoul_real_estate.csv'
+        file_path = 'seoul_housing/data/seoul_real_estate.csv'
     data = pd.read_csv(file_path)
     return data
