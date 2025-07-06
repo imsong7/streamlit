e3be14b959faa01a2144437fccd2f3c1a3d0c4cf
imsong7 <songsoo409@gmail.com>
2025-07-06 19:19:56 +0900

    “Updated”

--- seoul_housing/kr/eda/stat.py
+++ seoul_housing/kr/eda/stat.py
@@ -266,7 +266,7 @@ def regRession(total_df):
     
     # 해석 문장
     slope_text = f"건물면적이 1㎡ 증가할 때 아파트 가격은 평균적으로 약 <span style='color:red;'>{slope}만 원</span> 증가합니다."
-    st.markdown("#### 📌 회귀계수 해석")
+    st.markdown("#### 회귀계수 해석")
     st.markdown(slope_text, unsafe_allow_html=True)
 
 
